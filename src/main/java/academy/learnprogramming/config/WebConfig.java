81fe4483828e4003f0be0210da7ec19bcb1818f5
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2019-11-26 09:30:29 +0100

    Dodanie view resolvera (jako beana), drugiego endpointu i widoku jsp

--- src/main/java/academy/learnprogramming/config/WebConfig.java
+++ src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -1,8 +1,12 @@
 package academy.learnprogramming.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
 @EnableWebMvc //ta adnotacja odpowiada za uruchomienie całego mechanizmu MVC (view resolverów, request mappingów itp...)
 // dzieje się to za pomocą rejestrowania odpowiednich beanów  (specyficznych dla MVC) w kontekście całej aplikacji
@@ -12,4 +16,17 @@ public class WebConfig {
 
     //klasa - konfiguracja webowej części naszej aplikacji
 
+    // constants
+    public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
+    public static final String RESOLVER_SUFFIX = ".jsp";
+
+    //utworzenie i konfiguracje view resolvera
+    //robimy to poprzez stworzenei odpowiedniego beana
+    @Bean
+    public ViewResolver viewResolver() {
+        UrlBasedViewResolver viewResolver = new InternalResourceViewResolver(); //wybieramy sobie taki type view resolvera
+        viewResolver.setPrefix(RESOLVER_PREFIX);                                //ponadto obsługuje on JSTL
+        viewResolver.setSuffix(RESOLVER_SUFFIX);
+        return viewResolver;
+    }
 }
--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -7,9 +7,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DemoController {
 
-    @GetMapping("/hello")
+    @GetMapping("/hello") //http://localhost:8080/todo-list/hello
     @ResponseBody //oznacza, że to co zwróci ta metoda ma być DOSŁOWNIE zwrócone do przeglądarki, czyli w tym przypadku będzi to słówko "hello", a nie widok (thymeleaf, jsp itp...) o nazwie "hello"
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("welcome") //http://localhost:8080/todo-list/welcome
+    public String welcome() {
+        return "welcome";
     }
 }
