512a6e46f3d888b0ff3eeb5c260b1825cad980f6
Bartłomiej Mytko <bartlomiej.mytko@komputronik.pl>
2020-03-26 12:04:36 +0100

    Dodanie strony powitalnej z linkiem do stony z listą obiektów, dodanie linków do dodawania i usuwania obiektow

--- src/main/java/academy/learnprogramming/config/WebConfig.java
+++ src/main/java/academy/learnprogramming/config/WebConfig.java
@@ -1,10 +1,13 @@
 package academy.learnprogramming.config;
 
+import academy.learnprogramming.util.ViewNames;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
 
@@ -12,9 +15,12 @@ import org.springframework.web.servlet.view.UrlBasedViewResolver;
 // dzieje się to za pomocą rejestrowania odpowiednich beanów  (specyficznych dla MVC) w kontekście całej aplikacji
 @Configuration
 @ComponentScan(basePackages = "academy.learnprogramming")
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     //klasa - konfiguracja webowej części naszej aplikacji
+
+    //interfejs WebMvcConfigurer - defines callbacks methods to customize the java based configuration for spring mvc (which was enabled by the @EnableWebMvc annotation)
+    //zawiera domyślne metody, które można zaimplementować do dostosowania naszej konfiguracji, jedną z tych metod jest metoda addViewControllers
 
     // constants
     public static final String RESOLVER_PREFIX = "/WEB-INF/view/";
@@ -28,5 +34,13 @@ public class WebConfig {
         viewResolver.setPrefix(RESOLVER_PREFIX);                                //ponadto obsługuje on JSTL
         viewResolver.setSuffix(RESOLVER_SUFFIX);
         return viewResolver;
+    }
+
+    //jest używana do prostego, automatycznego dodawania kontrolerów, które będą pre-konfigurowane (razem z response statusem lub określonym widokiem jsp do zrenderowania)
+    //przydatne jeśli endpoint i tak by nic nie robił ponad zwrócenie stringa z nazwą odpowiedniego widoku jsp
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/").setViewName(ViewNames.HOME); //nie trzeba dodawać endpointu do żadnego kontrolera (lub nowego kontrolera)!
     }
 }
--- src/main/webapp/WEB-INF/view/items_list.jsp
+++ src/main/webapp/WEB-INF/view/items_list.jsp
@@ -1,4 +1,6 @@
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page import="academy.learnprogramming.util.Mappings" %>
+
 <html>
 <head>
     <title>Todo Items</title>
@@ -6,16 +8,27 @@
 <body>
 
     <div align="center">
+
+        <c:url var="addUrl" value="${Mappings.ADD_ITEM}" />
+        <h2><a href="${addUrl}">New Items</a></h2>
+
         <table border="1" cellpadding="5">
             <caption><h2>Todo Items</h2></caption>
             <tr>
                 <th>Title</th>
                 <th>Deadline</th>
+                <th>Delete</th>
             </tr>
             <c:forEach var="item" items="${todoData.items}">
+
+                            <c:url var="deleteUrl" value="${Mappings.DELETE_ITEM}"> <%-- to wyprodukuje url razem z parametrem (id obiektu do usunięcia) --%>
+                                <c:param name="id" value="${item.id}"/>
+                            </c:url>
+
                             <tr>
                                 <td><c:out value="${item.title}"/></td>
                                 <td><c:out value="${item.deadline}"/></td>
+                                <td><a href="${deleteUrl}">Delete</a></td>
                             </tr>
             </c:forEach>
         </table>
